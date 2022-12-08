6f93af3c3466698a5efd740c6f0291b7589eb27a
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-12-09 06:17:47 +0700

    Komentar di Golang

---
+++ go-comments/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Ini adalah komentar satu baris
+	// fmt.Println("Hello World!")
+	fmt.Println("Ini adalah Kode Alternatif")
+
+	/*
+		Kode di bawah ini akan menampilkan Hello World!
+		ke layar, ini adalah komentar banyak baris
+	*/
+	fmt.Println("Hello World!")
+
+	// Kode di bawah ini akan menampilkan Hello World!
+	// ke layar, ini adalah komentar banyak baris
+	fmt.Println("Hello World!")
+}
