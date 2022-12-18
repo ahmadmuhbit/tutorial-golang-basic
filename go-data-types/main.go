9904b7e1a188a49b0b85e3113c0496dad0e6c06a
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-12-18 07:00:14 +0700

    Tipe Data di Golang

---
+++ go-data-types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isMarried bool = true          // Boolean
+	var myAge int = 28                 // Integer
+	var pi float32 = 3.14              // Floating point
+	var myName string = "Ahmad Muhbit" // String
+
+	fmt.Println("Boolean :", isMarried)
+	fmt.Println("Integer :", myAge)
+	fmt.Println("Float   :", pi)
+	fmt.Println("String  :", myName)
+}
