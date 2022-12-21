319e29901c88bfe8adb2b4957ec46810d44184b4
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-12-22 06:32:01 +0700

    #9 Tipe Data Integer

---
+++ go-integer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 600
+	var b int = -4600
+
+	var c uint = 256
+	var d uint = 4600
+
+	fmt.Printf("Tipe data: %T, value: %v\n", a, a)
+	fmt.Printf("Tipe data: %T, value: %v\n", b, b)
+
+	fmt.Printf("Tipe data: %T, value: %v\n", c, c)
+	fmt.Printf("Tipe data: %T, value: %v\n", d, d)
+}
