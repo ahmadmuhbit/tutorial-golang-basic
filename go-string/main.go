da2089340a25d47d91a0028170de206655a4b0fb
ahmadmuhbit <ahmadmuhbit@gmail.com>
2022-12-24 06:03:56 +0700

    #11 Tipe Data String

---
+++ go-string/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myName string = "Ahmad Muhbit"
+	var myAddress string
+	nickName := "Ubit"
+
+	fmt.Printf("Value: %v, tipe data: %T\n", myName, myName)
+	fmt.Printf("Value: %v, tipe data: %T\n", myAddress, myAddress)
+	fmt.Printf("Value: %v, tipe data: %T\n", nickName, nickName)
+}
